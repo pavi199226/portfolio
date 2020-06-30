1b52ad7cf1e688a22e9d50ee661c28c94b931c5b
pavi199226 <pavi199226@gmail.com>
2020-06-30 09:40:45 +0200

    Updates

---
+++ precache-manifest.35f14adfa5ddd03cc37ecfe783ae2953.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56560334926fe1fe660053e0f03d0b00",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "13a66336f94ce25f58e8",
+    "url": "/portfolio/static/css/main.173868b5.chunk.css"
+  },
+  {
+    "revision": "332ae0f4d8a544dbbcf8",
+    "url": "/portfolio/static/js/2.52944bd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.52944bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a66336f94ce25f58e8",
+    "url": "/portfolio/static/js/main.49a9e3e9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "6a5e617a4b0e84cc689f7df2153e80ec",
+    "url": "/portfolio/static/media/avataars.6a5e617a.svg"
+  },
+  {
+    "revision": "56ec4ee72a91e8f3d954db6cccbf8ab2",
+    "url": "/portfolio/static/media/cabin.56ec4ee7.png"
+  },
+  {
+    "revision": "a29c0b43958bf3638364e15ff646fffa",
+    "url": "/portfolio/static/media/cake.a29c0b43.png"
+  },
+  {
+    "revision": "e27c88e555910ddce50b6f1e275023a4",
+    "url": "/portfolio/static/media/circus.e27c88e5.png"
+  },
+  {
+    "revision": "107d31e81b23d07ad38c7cc4d1f7ea27",
+    "url": "/portfolio/static/media/game.107d31e8.png"
+  },
+  {
+    "revision": "dfa7703b841989c158a12839d3f39e85",
+    "url": "/portfolio/static/media/img2.dfa7703b.jpg"
+  },
+  {
+    "revision": "7dfdcf559d331a0cdc05cc1d2b8c8233",
+    "url": "/portfolio/static/media/safe.7dfdcf55.png"
+  },
+  {
+    "revision": "8fad4ff92040b17345896e0e777a79ad",
+    "url": "/portfolio/static/media/submarine.8fad4ff9.png"
+  }
+]);
